5b9b39654d5361ca3fd6a1221d117bd05dcd1055
Ryan Li <ryan@knowble.com>
2022-01-11 20:35:53 +0000

    - added "dark mode" toggles, currently hardcoded to False (and `if False`s) - navbar expand button on small screen is now of the same height as the buttons on large screens - small layout changes to allow easier background switching of navbar and main section (navbar + section is now over 100% of screen height)

--- app/routes.py
+++ app/routes.py
@@ -5,8 +5,12 @@ from app import app
 
 @app.route('/')
 def index():
-    user = {'username': 'World'}
-    return render_template("login.html", title="Home", user=user)
+    return render_template("index.html")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
 
 @app.errorhandler(404)
