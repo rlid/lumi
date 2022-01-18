74846f5628cc94ff79a189ff25b93694591824f7
Ryan Li <ryan@knowble.com>
2022-01-18 19:48:30 +0000

    - added bootstrap-flask - WIP authentication framework

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap5
 
 from config import config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = "auth.login"
+bootstrap = Bootstrap5()
 
 
 def create_app(config_name):
@@ -16,6 +18,7 @@ def create_app(config_name):
 
     db.init_app(app)
     login_manager.init_app(app)
+    bootstrap.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class LoginForm(FlaskForm):
-   email = EmailField("Email address", validators=[DataRequired()])
-   password = PasswordField("Password", validators=[DataRequired()])
-   remember_me = BooleanField("Remember me")
-   submit = SubmitField("Log In")
+    email = EmailField("Email address", validators=[DataRequired()], render_kw={"placeholder": "name@example.com"})
+    password = PasswordField("Password", validators=[DataRequired(), Length(8)],
+                             render_kw={"placeholder": "Password"})
+    remember_me = BooleanField("Remember me")
+    submit = SubmitField("Log In", render_kw={"class": "w-100"})
