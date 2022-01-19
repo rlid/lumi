b71f7412d647f620beb04fa13676f6bba8e9e817
Ryan Li <ryan@knowble.com>
2022-01-19 09:22:47 +0000

    - WIP authentication framework

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import EmailField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import EmailField, PasswordField, BooleanField, SubmitField, Label
+from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
     email = EmailField("Email address", validators=[DataRequired()], render_kw={"placeholder": "name@example.com"})
-    password = PasswordField("Password", validators=[DataRequired(), Length(8)],
+    password = PasswordField("Password(*)", validators=[],
                              render_kw={"placeholder": "Password"})
-    remember_me = BooleanField("Remember me")
+    remember_me = BooleanField("Remember me", default=True)
     submit = SubmitField("Log In", render_kw={"class": "w-100"})
