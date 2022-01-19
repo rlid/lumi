4532ea72a2b85ab80cc40c511b4cc99160ba64de
Ryan Li <ryan@knowble.com>
2022-01-19 11:28:42 +0000

    - WIP authentication framework - added sign up - added message flashing

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,11 +1,32 @@
 from flask_wtf import FlaskForm
-from wtforms import EmailField, PasswordField, BooleanField, SubmitField, Label
-from wtforms.validators import DataRequired
+from wtforms import EmailField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, EqualTo, ValidationError
+
+from app.models import User
 
 
-class LoginForm(FlaskForm):
+class LogInForm(FlaskForm):
     email = EmailField("Email address", validators=[DataRequired()], render_kw={"placeholder": "name@example.com"})
     password = PasswordField("Password(*)", validators=[],
                              render_kw={"placeholder": "Password"})
     remember_me = BooleanField("Remember me", default=True)
     submit = SubmitField("Log In", render_kw={"class": "w-100"})
+
+
+class SignUpForm(FlaskForm):
+    email = EmailField("Email address", validators=[DataRequired()], render_kw={"placeholder": "name@example.com"})
+    password = PasswordField("Password(*)",
+                             validators=[EqualTo("confirm_password", message="Passwords must match.")],
+                             render_kw={"placeholder": "Password"})
+    confirm_password = PasswordField("Confirm password(*)", validators=[],
+                                     render_kw={"placeholder": "Confirm Password"})
+    submit = SubmitField("Register", render_kw={"class": "w-100"})
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError("The email address is already registered.")
+
+    def validate_password(self, field):
+        n = len(field.data)
+        if 0 < n < 8:
+            raise ValidationError("Password must be at least 8 characters long.")
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,49 @@
-from flask import render_template
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_user, logout_user, login_required
 
-from .forms import LoginForm
-from . import auth
+from app import db
+from app.auth import auth
+from app.models import User
+from app.auth.forms import LogInForm, SignUpForm
 
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
-    form = LoginForm()
+    form = LogInForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None:
+            if user.password_hash and user.verify_password(form.password.data):
+                login_user(user, form.remember_me.data)
+                next_url = request.args.get("next")
+                if next_url is None or not next_url.startwith("/"):
+                    next_url = url_for("main.index")
+                return redirect(next_url)
+            elif not form.password.data:
+                flash("Passwordless login", "info")
+                return redirect(url_for("main.index"))
+        flash("Invalid username or password", "danger")
     return render_template("auth/login.html", form=form)
+
+
+@auth.route("/logout", methods=["GET", "POST"])
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out.", "success")
+    return redirect(url_for("main.index"))
+
+
+@auth.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        if form.password.data:
+            user = User(email=form.email.data, password=form.password.data)
+        else:
+            user = User(email=form.email.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("You can now log in.")
+        return redirect(url_for("auth.login"))
+    return render_template("auth/signup.html", form=form)
