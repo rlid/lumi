293611cd088ea6b8c6be6dc50853519800fca9b9
Ryan Li <ryan@knowble.com>
2022-03-02 23:41:25 +0000

    - completed Cookie Policy - implemented Cookie Notice

--- app/main/views_nav.py
+++ app/main/views_nav.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, flash, Markup, url_for, session
 from flask_login import login_required
 
 from app.main import main
@@ -6,45 +6,68 @@ from app.main import main
 
 @main.route('/')
 def index():
-    return render_template("landing.html")
+    if session.get('allow_cookie') is None:
+        flash(Markup('<div class="toast-header bg-light"><strong class="me-auto">Cookie Notice</strong><button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button></div>') + \
+              Markup(
+                  '<div class="toast-body"><p class="mb-1">This site only uses essential cookies by default. ' +
+                  'Allow non-essential cookies?</p><div style="font-size:1.1rem">' +
+                  f'<a class="badge btn-primary text-decoration-none" href={url_for("main.allow_cookie", choice=1)}>' +
+                  'Accept</a> ' +
+                  f'<a class="badge btn-primary text-decoration-none" href={url_for("main.allow_cookie", choice=0)}>' +
+                  'Reject</a> ' +
+                  f'<a class="badge btn-primary text-decoration-none" href={url_for("main.cookie")}>' +
+                  'Learn More</a></div></div>')
+              , category='privacy')
+    return render_template('landing.html')
+
+
+@main.route('/allow_cookie/<int:choice>')
+def allow_cookie(choice):
+    session['allow_cookie'] = choice == 1
+    return redirect(url_for('main.index'))
+
+
+@main.route('/whatsapp')
+def whatsapp():
+    return redirect('https://api.whatsapp.com/send?phone=+447774523701', code=302)
 
 
 @main.route('/how-it-works')
 def how_it_works():
-    return render_template("landing.html", title="How It Works")
+    return render_template('landing.html', title='How It Works')
 
 
 @main.route('/privacy')
 def privacy():
-    return render_template("privacy.html", title="Privacy Policy")
+    return render_template('privacy.html', title='Privacy Policy')
 
 
 @main.route('/cookie')
 def cookie():
-    return render_template("cookie.html", title="Cookie Policy")
+    return render_template('cookie.html', title='Cookie Policy')
 
 
 @main.route('/alerts')
 @login_required
 def alerts():
-    return render_template("landing.html", title="Alerts")
+    return render_template('landing.html', title='Alerts')
 
 
 @main.route('/engagements')
 @login_required
 def engagements():
-    return render_template("landing.html", title="Active Engagements")
+    return render_template('landing.html', title='Active Engagements')
 
 
 @main.route('/saved')
 @login_required
 def saved():
-    return render_template("landing.html", title="Saved")
+    return render_template('landing.html', title='Saved')
 
 
 @main.route('/guidelines')
 def guidelines():
-    return render_template("landing.html", title="Community Guidelines")
+    return render_template('landing.html', title='Community Guidelines')
 
 
 @main.route('/about')
