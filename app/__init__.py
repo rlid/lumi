4651d78ba663826177769b4f1516cdc8fb0ca4c9
Ryan Li <ryan@knowble.com>
2022-03-04 11:08:30 +0000

    trying alternatives to make Sign-In with Apple work, attempt 6

--- app/__init__.py
+++ app/__init__.py
@@ -53,14 +53,14 @@ def create_app(config_name):
     moment.init_app(app)
     mobility.init_app(app)
     socketio.init_app(app)
-    # talisman.init_app(
-    #     app,
-    #     content_security_policy=csp,
-    #     content_security_policy_report_only=True,
-    #     content_security_policy_report_uri='/csp-report',
-    #     session_cookie_secure=False,
-    #     session_cookie_http_only=False
-    # )
+    talisman.init_app(
+        app,
+        content_security_policy=csp,
+        content_security_policy_report_only=True,
+        content_security_policy_report_uri='/csp-report',
+        session_cookie_secure=False,
+        session_cookie_http_only=False
+    )
 
     stripe.api_key = app.config['STRIPE_SECRET_KEY']
 
