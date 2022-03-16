270b092e63fa4435cca0b64b1ef7a06bd3a6f61a
Ryan Li <ryan@knowble.com>
2022-03-16 00:16:02 +0000

    - implemented QR code for sharing link - added button to copy referrer's link to clipboard - updated Open Graph metadata

--- main.py
+++ main.py
@@ -30,5 +30,5 @@ def test():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='192.168.0.8', port=8080, debug=True)
+    socketio.run(app, port=8080, debug=True)
     # app.run(host="0.0.0.0", port=8080, debug=True)
