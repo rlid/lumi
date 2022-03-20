926ccded044c176abc2002f70be3a1f5f0e19740
Ryan Li <ryan@knowble.com>
2022-03-20 12:54:59 +0000

    - added enviroment variable for google analytics

--- config.py
+++ config.py
@@ -16,16 +16,12 @@ class Config:
     SITE_RID_NBYTES = 32
     SITE_RID_HASH_DIGEST_SIZE = 32
 
-    SECRET_KEY = os.environ.get('SECRET_KEY', '')  # environment variable
-    STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')  # environment variable
-    STRIPE_WEBHOOK_SECRET = os.environ.get('STRIPE_WEBHOOK_SECRET', '')  # environment variable
-    GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET', '')  # environment variable
-    APPLE_CLIENT_SECRET = os.environ.get('APPLE_CLIENT_SECRET', '').replace('|', '\n')  # environment variable
-    # For Sign in with Apple, the client secret is dynamic - it is a JWT generated using a private key issued by Apple.
-    # This variable stores the private key, and this convention is assumed by Authlib and auth_utils.ApplePrivateKeyJWT
+    EMAIL_SENDER = utils.email.send_email_aws if os.environ.get('EMAIL_SENDER') == 'AWS' else (
+        utils.email.send_email_sg if os.environ.get('EMAIL_SENDER') == "SendGrid" else
+        utils.email.send_email_dummy
+    )
 
-    FORCE_HTTPS = os.environ.get('FORCE_HTTPS', '').lower() == 'true'
-
+    GOOGLE_ANALYTICS_MEASUREMENT_ID = os.environ.get('GOOGLE_ANALYTICS_MEASUREMENT_ID', 'G-1NZF9TXT5X')
     GOOGLE_CLIENT_ID = os.environ.get(
         'GOOGLE_CLIENT_ID',
         '175206125409-9kun97nija8cvt0k8f71j8cb4vidb8n4.apps.googleusercontent.com'
@@ -34,14 +30,19 @@ class Config:
     APPLE_TEAM_ID = os.environ.get('APPLE_TEAM_ID', '3WT485YTP5')
     APPLE_KEY_ID = os.environ.get('APPLE_KEY_ID', 'V79GWHFDH4')
 
+    FORCE_HTTPS = os.environ.get('FORCE_HTTPS', '').lower() == 'true'
+
+    SECRET_KEY = os.environ.get('SECRET_KEY', '')  # environment variable
+    STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')  # environment variable
+    STRIPE_WEBHOOK_SECRET = os.environ.get('STRIPE_WEBHOOK_SECRET', '')  # environment variable
+    GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET', '')  # environment variable
+    APPLE_CLIENT_SECRET = os.environ.get('APPLE_CLIENT_SECRET', '').replace('|', '\n')  # environment variable
+    # For Sign in with Apple, the client secret is dynamic - it is a JWT generated using a private key issued by Apple.
+    # This variable stores the private key, and this convention is assumed by Authlib and auth_utils.ApplePrivateKeyJWT
+
     STRIPE_PRICE_5 = os.environ.get('STRIPE_PRICE_5', 'price_1KWgD0GoAMQGbjHHUBRINR2I')
     STRIPE_PRICE_10 = os.environ.get('STRIPE_PRICE_10', 'price_1KWkE2GoAMQGbjHHKOP6j5Be')
     STRIPE_PRICE_20 = os.environ.get('STRIPE_PRICE_20', 'price_1KWkEhGoAMQGbjHHdwIdNHvO')
-
-    EMAIL_SENDER = utils.email.send_email_aws if os.environ.get('EMAIL_SENDER') == 'AWS' else (
-        utils.email.send_email_sg if os.environ.get('EMAIL_SENDER') == "SendGrid" else
-        utils.email.send_email_dummy
-    )
 
     @staticmethod
     def init_app(app):
